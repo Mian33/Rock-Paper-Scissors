0f321f3fc50b9c2b738471cf5f779df0e2268965
Ian McFedries <imcfedries24@biomedscienceacademy.org>
2020-12-17 18:50:04 +0000

    I added extra commands and fixed all bugs

--- main.py
+++ main.py
@@ -16,85 +16,86 @@ print("If you draw then the number of lives stays the same.")
 print("Type \"display lives\" at any time to see how many lives you have left")
 print("Type \"display score\" at any time to see your score")
 print("Type \"display drew\" at any time to see your score")
+print("Type \"exit\" at any time to exit the code")
 print("Make sure to not use any capitals when playing.")
 print("Can you win?")
 print("Good luck!")
 print("")
 print("")
-
-while true:
+true = 1
+while true == 1:
   rps = input("rock, paper, or scissors?   ")
   import random
   computer = ("rock", "paper", "scissors")
   computer = random.choice(computer)
   #Here are the if statements for rock
   if rps == "rock" and computer == "paper":
-    print("The computer chose" + str(computer))
+    print("")
+    print("The computer chose " + str(computer))
     print("")
     print(loose)
     print("")
-    print("")
     lives -= 1
   if rps == "rock" and computer == "scissors":
-    print("The computer chose" + str(computer))
+    print("")
+    print("The computer chose " + str(computer))
     print("")
     print(win)
     computer_lives -= 1
-    print("")
     print("")
     score += 1
   #Here are the if statements for paper
   if rps == "paper" and computer == "scissors":
-    print("The computer chose" + str(computer))
+    print("")
+    print("The computer chose " + str(computer))
     print("")
     print(loose)
     print("")
-    print("")
     lives -= 1
   if rps == "paper" and computer == "rock":
-    print("The computer chose" + str(computer))
+    print("")
+    print("The computer chose " + str(computer))
     print("")
     print(win)
     computer_lives -= 1
-    print("")
     print("")
     score += 1
   #Here are the if statements for scissors
   if rps == "scissors" and computer == "paper":
-    print("The computer chose" + str(computer))
+    print("")
+    print("The computer chose " + str(computer))
     print("")
     print(win)
     computer_lives -= 1
     print("")
-    print("")
     score += 1
   if rps == "scissors" and computer == "rock":
-    print("The computer chose" + str(computer))
+    print("")
+    print("The computer chose " + str(computer))
     print("")
     print(loose)
-    print("")
     print("")
     lives -= 1
   #Here are the if statements for duplicates
   if rps == "rock" and computer == "rock":
-    print("The computer chose" + str(computer))
+    print("")
+    print("The computer chose " + str(computer))
     print("")
     print("You drew")
-    print("")
     print("")
     drew += 1
   if rps == "paper" and computer == "paper":
-    print("The computer chose" + str(computer))
+    print("")
+    print("The computer chose " + str(computer))
     print("")
     print("You drew")
-    print("")
     print("")
     drew += 1
   if rps == "scissors" and computer == "scissors":
-    print("The computer chose" + str(computer))
+    print("")
+    print("The computer chose " + str(computer))
     print("")
     print("You drew")
-    print("")
     print("")
     drew += 1
   #commands
@@ -108,12 +109,14 @@ while true:
   if lives == 0 or rps == "test":
     print("You ran out of lives.")
     print("Thanks for playing!")
-    print("Your score is" + str(score) + ".")
-    print("You had " + str(drew) = " draws.")
+    print("Your score is " + str(score) + ".")
+    print("You had " + str(drew) + " draws.")
     stop = input("Press enter to exit.")
-  if computer_lives = 0:
+  if computer_lives == 0:
     print("The computer ran out of lives. You Win!")
     print("Thanks for playing!")
     print("Your score is" + str(score) + ".")
-    print("You had " + str(drew) = " draws.")
+    print("You had " + str(drew) + " draws.")
     stop = input("Press enter to exit.")
+  if rps == "exit":
+    true += 1
